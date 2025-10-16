6cba5f055d745dafa711d18698be4e40f73a4c55
Junghwan Kim <everydayeffort@naver.com>
2025-10-17 08:48:45 +0900

    2025/10/17

---
+++ 10211/10211.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    for(int t = 0; t<T; t++){
+        int n;
+        cin >> n;
+        vector<int> arr;
+        for(int i = 0; i<n; i++){
+            int num;
+            cin >> num;
+            arr.push_back(num);
+        }
+        int ans = arr[0];
+        int temp = arr[0];
+        for(int i = 1; i<n; i++){
+            temp = max(arr[i], temp + arr[i]);
+            ans = max(ans, temp);
+        }
+        cout << ans << endl;
+    }
+    
+}
